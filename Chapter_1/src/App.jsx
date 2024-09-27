2944a0e0ab0dd1bd73409ee5273c3ef6083a1af2
Nishant Zoc <zocnishant@gmail.com>
2024-09-27 12:12:47 +0545

    .

--- Chapter_1/src/App.jsx
+++ Chapter_1/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Jhyau Lagne kna ho malai.
         </p>
       </div>
       <p className="read-the-docs">
